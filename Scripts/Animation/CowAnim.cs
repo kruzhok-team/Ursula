35db57b1212bfdc5798cfced180d61dceee6bd39
Belias5 <55509214+Belias5@users.noreply.github.com>
2025-01-27 18:33:11 +0300

    Create CowAnim.cs

---
+++ Scripts/Animation/CowAnim.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+using System;
+
+public partial class CowAnim : BaseAnimation
+{   
+
+    public override async void PlayUseAnimation()
+    {
+        State = state.Use;
+        PlayEatingAnimation();
+        //PlayHeadSpinAnimation();
+    }
+
+
+}
