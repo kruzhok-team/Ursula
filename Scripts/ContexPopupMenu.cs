38f503692c8cf8803315b65f3faac3d6fcfb907a
Belias5 <55509214+Belias5@users.noreply.github.com>
2025-01-27 18:33:07 +0300

    Create ContexPopupMenu.cs

---
+++ Scripts/ContexPopupMenu.cs
@@ -0,0 +1,40 @@
+﻿using Godot;
+using System;
+
+public partial class ContexPopupMenu : Button
+{
+    bool state = false;
+
+    [Export] Panel PanelMenu;
+    [Export] TextureRect arrow1;
+    [Export] TextureRect arrow2;
+
+    public void OnClick()
+    {
+        bool newState = !state;
+        ControlPopupMenu.HideAllMenu();
+        state = newState;
+        SetState();
+    }
+
+    public void SetState()
+    {
+        if (PanelMenu != null) PanelMenu.Visible = state;
+        arrow1.Visible = state;
+        arrow2.Visible = !state;
+    }
+
+    public void Show()
+    {
+        state = true;
+        SetState();
+    }
+
+    public void Hide()
+    {
+        state = false;
+        SetState();
+    }
+
+
+}
