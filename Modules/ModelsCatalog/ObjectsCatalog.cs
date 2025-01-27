c642eff7658d3dedf427c8a887a3763e98b6d67d
Belias5 <55509214+Belias5@users.noreply.github.com>
2025-01-27 18:33:31 +0300

    Create ObjectsCatalog.cs

---
+++ Modules/ModelsCatalog/ObjectsCatalog.cs
@@ -0,0 +1,17 @@
+﻿using Godot;
+using System;
+
+public partial class ObjectsCatalog : Node
+{
+    [Export] Control panelLoadObject;
+
+    public void OnOpenPanelLoadObject()
+    {
+        panelLoadObject.Visible = true;
+    }
+
+    public void OnClosePanelLoadObject()
+    {
+        panelLoadObject.Visible = false;
+    }
+}
