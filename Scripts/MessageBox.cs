ac4fdf4b69883b9e835e85dfec9f8569afee2863
Belias5 <55509214+Belias5@users.noreply.github.com>
2025-01-27 18:32:36 +0300

    Create MessageBox.cs

---
+++ Scripts/MessageBox.cs
@@ -0,0 +1,30 @@
+﻿using Godot;
+
+
+public partial class MessageBox : Control
+{
+    public static MessageBox instance;
+    [Export] Label label;
+
+    public override void _Ready()
+    {
+        instance = this;
+    }
+
+    public void ShowMessage(string message)
+    {
+        if (label == null) return;
+
+        label.Text = message;
+
+        this.Visible = true;
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton eventMouseButton && eventMouseButton.Pressed)
+        {
+            this.Visible = false;
+        }
+    }
+}
