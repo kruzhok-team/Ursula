d53e3e82e5875151f0946ead0f30f006c20809f2
Belias5 <55509214+Belias5@users.noreply.github.com>
2025-01-27 18:32:24 +0300

    Create VoxStruct.cs

---
+++ Scripts/VoxStruct.cs
@@ -0,0 +1,12 @@
+﻿using Godot;
+using System;
+
+public enum GameItemRotation : byte { forward = 0, right = 1, left = 2, backward = 3, up = 4, down = 5 };
+public enum VoxDrawTypes : int { solid, water, lava, ice, swamp };
+
+public partial class VoxStruct : Node
+{
+
+}
+
+
