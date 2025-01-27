a0528212c1ac3678523a2337aa9ca9185419439e
Belias5 <55509214+Belias5@users.noreply.github.com>
2025-01-27 18:33:00 +0300

    Create ControlTabPanel.cs

---
+++ Scripts/ControlTabPanel.cs
@@ -0,0 +1,32 @@
+﻿using Godot;
+using System;
+
+public partial class ControlTabPanel : Panel
+{
+    [Export]
+    public ContexPopupMenu[] panels;
+
+    private int current = 0;
+
+    public override void _EnterTree()
+    {
+        HideAll();
+        OpenPanel(current);
+    }
+
+    void HideAll()
+    {
+        for (int i = 0; i < panels.Length; i++)
+        {
+            //panels[i].Visible = false;
+            panels[i].Hide();
+        }
+    }
+
+    public void OpenPanel(int arg)
+    {
+        current = arg;
+        HideAll();
+        if (panels != null && current < panels.Length) panels[current].Show();
+    }
+}
