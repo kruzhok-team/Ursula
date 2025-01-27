87372633b5359e57d6d63ffed95417398289da65
Belias5 <55509214+Belias5@users.noreply.github.com>
2025-01-27 18:32:50 +0300

    Create GameManager.cs

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using Godot;
+using System;
+
+public partial class GameManager : Node
+{
+    public static Action onPlayerInteractionObjectAction;
+
+    public override void _Ready()
+    {
+        VoxLib.GM ??= this;
+    }
+}
