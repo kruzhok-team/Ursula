7512b18e7a2b9cabb5e22d99a51649ffc1cfd74f
Belias5 <55509214+Belias5@users.noreply.github.com>
2025-01-27 18:32:45 +0300

    Create ItemBase.cs

---
+++ Scripts/ItemBase.cs
@@ -0,0 +1,12 @@
+﻿using Godot;
+using System;
+
+public partial class ItemBase : Node3D
+{
+	public bool magicItem;
+	public bool canTake;
+	public bool canMove;
+	public bool canRotate;
+	public bool canActivate;
+	public bool canSetup;
+}
