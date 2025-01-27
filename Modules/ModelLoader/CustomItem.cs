ae8dc5eea8ae7e7dc0a061833bed15cc32404e01
Belias5 <55509214+Belias5@users.noreply.github.com>
2025-01-27 18:33:49 +0300

    Create CustomItem.cs

---
+++ Modules/ModelLoader/CustomItem.cs
@@ -0,0 +1,42 @@
+﻿using Godot;
+using System;
+using System.IO;
+
+public partial class CustomItem : Node
+{
+    public string objPath = "";
+    public Node3D modelInstance;
+
+    public override void _Ready()
+    {
+        InitModel();
+    }
+
+    public async void InitModel()
+    {
+        await ToSignal(GetTree(), SceneTree.SignalName.PhysicsFrame);
+
+        if (modelInstance != null)
+            modelInstance.QueueFree();
+
+        if (string.IsNullOrEmpty(objPath))
+        {
+
+        }
+        else
+        {
+            modelInstance = ModelLoader.LoadModelByPath(objPath);
+            if (modelInstance == null) return;
+
+            this.AddChild(modelInstance);
+
+            Node parent = GetParent();
+
+            //ItemPropsScript ips = parent as ItemPropsScript;
+            ItemPropsScript ips = (ItemPropsScript)this.GetParent().FindChild("ItemPropsScript", true, true);
+
+            parent.Name = Path.GetFileNameWithoutExtension(objPath) + $"{ips.x}{ips.y}{ips.z}";
+        }
+    }
+
+}
