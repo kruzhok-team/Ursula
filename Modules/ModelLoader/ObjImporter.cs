f23ae717fe1c628192a4226fa155080bf31726e0
Belias5 <55509214+Belias5@users.noreply.github.com>
2025-01-27 18:33:35 +0300

    Create ObjImporter.cs

---
+++ Modules/ModelLoader/ObjImporter.cs
@@ -0,0 +1,36 @@
+﻿using Godot;
+using System;
+
+public partial class ObjImporter 
+{
+    //static GodotObject GetObjParserGD()
+    //{
+    //    GDScript MyGDScript = GD.Load<GDScript>("res://addons/VoxLib/Modules/ModelLoader/ObjParse.gd");
+    //    return (GodotObject)MyGDScript.New();
+    //}
+
+    //public static Mesh LoadObjFileAsMesh(string objPath, string mtlPath)
+    //{
+    //    //GDScript
+    //    //var importer = GetObjParserGD();
+    //    //return (Mesh)importer.Call("load_obj", objPath, mtlPath);
+
+    //    //C#
+    //    return (Mesh)ObjParser.LoadObj(objPath, mtlPath);
+    //}
+
+    public static Node3D LoadObjFile(string objPath, string mtlPath)
+    {
+        var mesh = (Mesh)ObjParser.LoadObj(objPath, mtlPath);
+
+        MeshInstance3D meshInstance = new MeshInstance3D();
+        meshInstance.Mesh = mesh;
+
+        var instance = new Node3D();
+        instance.Name = "Model";
+
+        instance.AddChild(meshInstance);
+
+        return instance;
+    }
+}
