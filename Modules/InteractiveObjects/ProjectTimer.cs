309732fe38cf028086f11474c1922af50308514f
Belias5 <55509214+Belias5@users.noreply.github.com>
2025-01-27 18:33:54 +0300

    Create ProjectTimer.cs

---
+++ Modules/InteractiveObjects/ProjectTimer.cs
@@ -0,0 +1,48 @@
+﻿using Godot;
+using System;
+
+public partial class ProjectTimer : Node
+{
+    private static ProjectTimer _instance;
+
+    public static ProjectTimer Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    public GMLActionHolder Tick = new(nameof(Tick));
+    public GMLActionHolder TickOneSecond = new(nameof(TickOneSecond));
+
+    private Timer _timer;
+
+    private const float GameTick = 0.05f;
+    private float _accumulatedTime = 0.0f;
+
+    public override void _Ready()
+    {
+        _instance = this;
+
+        _timer = new Timer();
+        _timer.WaitTime = GameTick;
+        _timer.OneShot = false;
+        _timer.Timeout += OnTick;
+        AddChild(_timer);
+
+        _timer.Start();
+    }
+
+    private void OnTick()
+    {
+        _accumulatedTime += GameTick;
+        Tick.Invoke();
+
+        if (_accumulatedTime >= 1.0f)
+        {
+            _accumulatedTime -= 1.0f;
+            TickOneSecond.Invoke();
+        }
+    }
+}
